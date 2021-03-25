28eaeabebc9d2de2127fe4dfd60d0348240f4de4
Leonardo <focuux@gmail.com>
2021-03-25 07:43:31 -0400

    Initial Commit

---
+++ Shared/AutoClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoClickerApp.swift
+//  Shared
+//
+//  Created by Leonardo Dominguez on 25/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AutoClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
